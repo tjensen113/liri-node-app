38d56b8c82d3974f66e2467a39d79f160603bea7
Thomas Jensen <thomasjensen@Thomass-MacBook-Pro.local>
2019-06-14 22:37:34 -0500

    switch statment

--- liri.js
+++ liri.js
@@ -1,6 +1,27 @@
 require("dotenv").config();
+var spotify = new Spotify(keys.spotify);
 var keys = require("./keys.js");
 var fs = require("fs");
 
-// var spotify = new Spotify(keys.spotify);
+ß
 // how to reference the spotify keys with code line above
+
+var spotifyThis = require("./spotify.js");
+var myMovies = require("./movies.js");
+var Concert = require("./concerts.js");
+
+
+var userCommand=process.argv[2];
+var userInput=process.argv.splice(3,process.argv.length).join(' ');
+
+
+
+
+
+switch (userCommand){
+    case "concert-this":
+        Concert(userInput);
+        break;
+    case "spotify-this":
+
+}
